cdb9259aeee23a4e80ae3ceb8a178d5519c57532
Michael Jiang <mjiang@lcs.on.ca>
2025-01-14 11:07:33 -0500

    Initial Commit

---
+++ FindTheRoots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindTheRoots
+//
+//  Created by Michael on 2025/1/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Michael on 2025/1/14.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
