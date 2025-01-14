b360d7c22ea6faab75995fcb52fbe0b3ed9158d6
Michael Jiang <mjiang@lcs.on.ca>
2025-01-14 12:02:11 -0500

    Finish the slider.

--- FindTheRoots/Views/RootsCalculatorView.swift
+++ FindTheRoots/Views/RootsCalculatorView.swift
@@ -7,18 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootsCalculatorView: View {
+    
+    @State var numbera: Double = 1
+    @State var numberb: Double = 1
+    @State var numberc: Double = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack(spacing: 20) {
+               
+                Image("1")
+                Image("2")
+                
+                HStack{
+                    VStack{
+                        
+                        Text("a: \(numbera)")
+                        
+                        Slider(
+                            value: $numbera,
+                            in: 1...100,
+                            step:(1)
+                        )
+                    }
+                    
+                    VStack{
+                        
+                    Text("b: \(numberb)")
+                        Slider(
+                            value: $numberb,
+                            in: 1...100,
+                            step:(1)
+                        )
+                    }
+                    
+                    VStack{
+                        
+                        Text("c: \(numberc)")
+                        
+                        Slider(
+                            value: $numberc,
+                            in: 1...100,
+                            step:(1)
+                        )
+                    }
+                        }
+                
+                Text("x ≈ 2.00 and x ≈ 4.00")
+            }
+            .navigationTitle("Find the Roots")
         }
-        .padding()
+        
+        
     }
+    
 }
 
+
 #Preview {
-    ContentView()
+    RootsCalculatorView()
 }
